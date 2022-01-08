44bc376c2eeb899642d90ad7a74cfd4af33ce0fe
Jahin Z <jahinongoogol@gmail.com>
2022-01-08 17:29:39 +1100

    Added columns and title clock

--- src/console.py
+++ src/console.py
@@ -12,11 +12,13 @@ Full license available at https://github.com/astanin/python-tabulate/blob/master
 """
 import os, sys
 from tabulate import tabulate
+from datetime import datetime
+from src.util import Util
 
 class Console:
 
     # class constant: default cmd window size (for supported platforms)
-    WINDOW_SIZE = [110, 50]
+    WINDOW_SIZE = [120, 50]
 
     # class constants: helper vars
     TAB = '\t'
@@ -33,15 +35,38 @@ class Console:
             "msys" : "cls"
         },
         "RESIZE" : {
-            "darwin" : "resize -s {} {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
+            "darwin" : "/usr/X11/bin/resize -s {} {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
             "linux" : "resize -s {} {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
             "linux2" : "resize -s {} {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
             "win32" : "MODE {}, {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
             "cygwin" : "MODE {}, {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1]),
             "msys" : "MODE {}, {}".format(WINDOW_SIZE[0], WINDOW_SIZE[1])
+        },
+        "TITLE" : {
+            "darwin" : "echo -n -e \"\\033]0;{}\\\007\"",
+            "linux" : "gnome-terminal --title='{}'",
+            "linux2" : "gnome-terminal --title='{}'",
+            "win32" : "title {}",
+            "cygwin" : "title {}",
+            "msys" : "title {}"
         }
     }
     
+    # class constant: Symbol lookup table
+    SYMBOLS = {
+
+        "1" : "BTC",
+        "2" : "ETH",
+        "3" : "BNB",
+        "4" : "SOL",
+        "5" : "ADA",
+        "6" : "XRP",
+        "7" : "DOT",
+        "8" : "LUNA",
+        "9" : "AVAX",
+        "10" : "DOGE",
+
+    }
 
     # internal storage for tables (sample data; will be reset with __init__)
     internalBook = {
@@ -111,10 +136,21 @@ class Console:
         singleTable = []
         for priceGroup in self.internalBook:
             singleTable += self.internalBook[priceGroup]
+
+        # Processing order status
+        for order in singleTable:
+            
+            # Process order status; default: open
+            order += "OPEN"
+
+            # Process symbol
+            order[3] = self.SYMBOLS[order[3]]
+
         
         # Clear and resize cmd/terminal window
         os.system(self.CMD_LIST["CLEAR"][sys.platform])
         os.system(self.CMD_LIST["RESIZE"][sys.platform])
-
+        os.system(self.CMD_LIST["TITLE"][sys.platform].format("Current Time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S (Server Timestamp " + str(Util.get_server_time())) + ")"))
+        
         # Print!
-        print(tabulate(singleTable, headers = ["Indicator", "Price", "Quantity", "Orderbook ID", "Time in Force", "Time Received", "Order ID", "Order Token"]))    
+        print(tabulate(singleTable, headers = ["Indicator", "Price", "Quantity", "Symbol", "Time in Force", "Time Received", "Order ID", "Order Token", "Order Status"]))    
